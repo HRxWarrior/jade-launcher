0499d8e4267e55a0c5319ed91329628c7c83bfe5
HRxWarrior <kinghr146@gmail.com>
2026-05-14 14:26:12 +0530

    Create AppInfo.kt

---
+++ app/src/main/java/com/hrishikesh/jadelauncher/models/AppInfo.kt
@@ -0,0 +1,9 @@
+package com.hrishikesh.jadelauncher.models
+
+import android.graphics.drawable.Drawable
+
+data class AppInfo(
+    val appName: String,
+    val packageName: String,
+    val icon: Drawable?
+)
